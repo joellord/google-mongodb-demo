338420dbcc2c237548f97eb15cf6915e11964a1a
Joel Lord <joelphy@gmail.com>
2023-01-05 12:01:31 -0500

    more gitignores

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
+import config from "./config.json";
+
 const settings = {
-  BASE_URL: "https://data.mongodb-api.com/app/application-0-hvzpr/endpoint",
-  // BASE_URL: "http://localhost:5050",
-  // BASE_URL: "https://data.mongodb-api.com/app/data-twslp/endpoint/data/v1/actions",
+  BASE_URL: config.BASE_URL,
   DATA_API_KEY: "U4UyHi0l6DVaLb9M7PMUpRLeyRXgcbuQnEm6mmOoZnuU17H4jpKin2VbwHI3tOS9",
   APP_ID: "application-0-hvzpr"
 }
